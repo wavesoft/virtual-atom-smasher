b9470b39fbc32e724ae0d727ee8e47989e3de2cb
wavesoft <wavesoft@github.com>
2015-02-16 13:44:15 +0100

    Importing project from LiveQ

---
+++ src/static/js/bootstrap.build.js
@@ -0,0 +1,12 @@
+({
+    baseUrl: "./lib",
+    name: "../bootstrap",
+    out: "bootstrap.min.js",
+    paths: {
+    	'core'         : '../vas-core.js',
+    	'liveq'        : '../liveq.js',
+    	'vas-3d'       : '../vas-3d.js',
+        'vas-basic'    : '../vas-basic.js',
+        'vas-editor'   : '../vas-editor.js'
+    }
+})
---
+++ src/static/js/liveq.js/LiveQ.js
@@ -0,0 +1,10 @@
+
+define({
+
+	/**
+	 * The LiveQ Client Library Version
+	 * @member {string}
+	 */
+	"version": "2.0",
+
+});
