bc6ccdf03c34600eac0683632ba27407335cebe8
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2015-03-02 12:09:57 +0100

    Final teaser version

--- drafts/teaser/res/ui.js
+++ drafts/teaser/res/ui.js
@@ -7,20 +7,63 @@ $(function() {
 		$("#pxback").css({
 			'top': bt
 		});
-		var rt = $("#register").position().top - bt;
-		$("#pxblur").css({
-			'top': -rt
-		});
 	}
 
 	function on_resize() {
 		$(".page-full").css({
 			'height': $(window).height()
 		});
+		$("#pxback").css({
+			'height': $(window).height()*1.25
+		});
 		on_scroll();
+	}
+
+	function register_email(email) {
+	    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		var e = email.trim();
+		if (!e || !re.test(email)) {
+			$("#register input").addClass("wrong").focus();
+			return;
+		}
+		$("#register .form").hide();
+
+		// Send ajax request
+		$.ajax({
+			url: "subscribe.php",
+			type: "POST",
+			data: { 'email' : email },
+			dataType: 'json'
+		})
+		.done(function(data) {
+				if (data['status'] != "ok") {
+					$("#register .thankyou")
+						.html("An error occured: " + data['message'])
+						.fadeIn();
+				} else {
+					$("#register .thankyou")
+						.html("You will be notified to <b>"+email+"</b> when the beta-testing opens!")
+						.fadeIn();
+				}
+			})
+		.fail(function() {
+			$("#register .thankyou")
+				.html("Registration failed due to a server error!")
+				.fadeIn();
+			});
+
 	}
 
 	on_resize();
 	$(window).resize(on_resize);
 	$(window).scroll(on_scroll);
+	$("#register button").click(function() {
+		register_email($("#register input").val());
+	});
+	$("#register input").keydown(function() {
+		$("#register input").removeClass("wrong");
+	});
+
+	$("#register .thankyou").hide();
+
 });
--- drafts/teaser/subscribe.php
+++ drafts/teaser/subscribe.php
@@ -1,5 +1,52 @@
 <?php
 
-header("Location: index.html");
+/* Server configuration */
+$server = "";
+$server_user = "";
+$server_pass = "";
+$server_db   = "";
+
+/* Connect to SQL */
+$mysqli = new mysqli($server, $server_user, $server_pass, $server_db);
+if ($mysqli->connect_errno) {
+	die(json_encode(array(
+			"status" => "error",
+			"message" => "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error
+		)));
+}
+
+/* If we have an e-mail, subscribe */
+if (isset($_POST['email'])) {
+
+	// Prepare insert statement
+	if (!($stmt = $mysqli->prepare("INSERT INTO teaser_users (email,ip,host,useragent) VALUES (?,?,?,?)"))) {
+		die(json_encode(array(
+				"status" => "error",
+				"message" => "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error
+			)));
+	}
+
+	// Bind parameters
+	if (!$stmt->bind_param("ssss", $_POST['email'], $_SERVER['REMOTE_ADDR'], gethostbyaddr($_SERVER['REMOTE_ADDR']), $_SERVER['HTTP_USER_AGENT'] )) {
+		die(json_encode(array(
+				"status" => "error",
+				"message" => "Binding parameters failed: (" . $mysqli->errno . ") " . $mysqli->error
+			)));
+	}
+
+	// Execute statement
+	if (!$stmt->execute()) {
+		die(json_encode(array(
+				"status" => "error",
+				"message" => "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error
+			)));
+	}
+
+	// OK!
+	echo(json_encode(array(
+			"status" => "ok"
+		)));
+
+}
 
 ?>
