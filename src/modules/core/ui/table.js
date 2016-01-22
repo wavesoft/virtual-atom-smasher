a58e5d0505bd2fd4f3441bd12c589d2d814af36b
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-01-23 00:13:32 +0100

    Adding sorting support on table controller

--- src/modules/core/ui/table.js
+++ src/modules/core/ui/table.js
@@ -18,6 +18,8 @@ define([], function() {
 		this.eListBody = $('<tbody></tbody>').appendTo(this.eListTable);
 
 		// Column definitions
+		this.sortOrder = false;
+		this.sortColumn = "";
 		this.columns = [];
 		this.rows = [];
 
@@ -26,7 +28,13 @@ define([], function() {
 	/**
 	 * Define the table columns
 	 */
-	Table.prototype.addColumn = function( id, title, size, renderer  ) {
+	Table.prototype.addColumn = function( id, title, size, align, renderer  ) {
+
+		// Slide renderer to align if missing
+		if (typeof(align) == 'function') {
+			renderer = align;
+			align = 'center';
+		}
 
 		// Create default renderer
 		if (!renderer) {
@@ -38,25 +46,107 @@ define([], function() {
 			}
 		}
 
+		// Pick align class
+		var alignClass = 'text-center';
+		if (align == 'left') {
+			alignClass = "";
+		} else if (align == 'center') {
+			alignClass = "text-center";
+		}
+
+		// Create header item
+		var hItem = $('<th class="col-'+size+' '+alignClass+' table-header-sortable"></th>').text(title);
+		hItem.click((function(e) {
+
+			// Pick sort column and order
+			if (this.sortColumn != id) {
+				this.sortColumn = id;
+				this.sortOrder = false;
+			} else {
+				// Flip sort order
+				this.sortOrder = !this.sortOrder;
+			}
+
+			// Apply sort
+			this.applySorting( this.sortColumn, this.sortOrder );
+
+		}).bind(this));
+
+		// Crete sort icon
+		var hIcon = $('<img />').hide().appendTo(hItem);
+
 		// Keep on column data
 		this.columns.push({
 			'id': id, 
 			'size': size,
-			'renderer': renderer
+			'renderer': renderer,
+			'align': alignClass,
+			'elm': hItem,
+			'elmIcon': hIcon,
 		});
 
 		// Add on header
-		this.eListHeader.append(
-			$('<th class="col-'+size+' text-center"></th>').text(title)
-		);
+		this.eListHeader.append( hItem );
 
+		// Chainable calls
+		return this;
 	}
 
 	/**
 	 * Apply the sorting function
 	 */
-	Table.prototype.applySorting = function() {
+	Table.prototype.applySorting = function( key, order ) {
+
+		// Get list items
+		var items = this.eListBody.children(),
+			self = this;
 		
+		// Sort			
+		items.sort(function(a,b){
+			var eA = self.rows[$(a).data("row")],
+				eB = self.rows[$(b).data("row")],
+				vA = eA[key], vB = eB[key];
+
+			// String comparison is case-insensitive
+			if ((typeof(vA) == "string") || (typeof(vB) == "string")) {
+				vA = String(vA).trim().toUpperCase();
+				vB = String(vB).trim().toUpperCase();
+			}
+
+			// Comparison
+			if (vA > vB) {
+				console.log("--",vA,">",vB)
+				return order ? 1 : -1;
+			} else if (vA < vB) {
+				console.log("--",vA,"<",vB)
+				return order ? -1 : 1;
+			} else {
+				console.log("--",vA,"=",vB)
+				return 0;
+			}
+
+		});
+
+		// Detach and re-append in order
+		items.detach().appendTo(this.eListBody);
+
+		// Update header icons
+		for (var i=0; i<this.columns.length; i++) {
+			var c = this.columns[i];
+			if (c.id == key) {
+				c.elm.addClass("table-header-active");
+				c.elmIcon.show();
+				if (order) {
+					c.elmIcon.attr("src", "modules/vas/basic/img/icons/bullet_arrow_up.png" );
+				} else {
+					c.elmIcon.attr("src", "modules/vas/basic/img/icons/bullet_arrow_down.png" );
+				}
+			} else {
+				c.elm.removeClass("table-header-active");
+				c.elmIcon.hide();
+			}
+		}
+
 	}
 
 	/**
@@ -65,14 +155,16 @@ define([], function() {
 	Table.prototype.add = function( rowData ) {
 		if (!rowData) return;
 		this.rows.push( rowData );
-		var row = $('<tr></tr>').appendTo(this.eListBody);
+		var row = $('<tr></tr>')
+					.data("row", this.rows.length-1)
+					.appendTo(this.eListBody);
 		for (var i=0; i<this.columns.length; i++) {
 			var c = this.columns[i];
 			row.append(
 				$('<td></td>')
 					.append( c.renderer(rowData[c.id], rowData) )
 					.addClass( "col-"+c.size )
-					.addClass( "text-center" )
+					.addClass( c.align )
 			);
 		}
 		return row;
